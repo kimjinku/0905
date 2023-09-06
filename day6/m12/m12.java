cd3c13e8044d401ac279d6c00ee76b3de0cd8386
kimjinku <kjk10271215@gmail.com>
2023-09-06 17:48:40 +0900

    0906

--- day6/m12/m12.java
+++ day6/m12/m12.java
@@ -11,7 +11,7 @@ public class m12 {
 
         흰오리 a흰오리 = new 흰오리();
         a흰오리.날다();
-        a흰오리.a고무오리.헤엄치다();
+        a흰오리.헤엄치다();
         // 출력 : 오리가 날개로 날아갑니다.
 
         고무오리 a고무오리 = new 고무오리();
@@ -25,16 +25,15 @@ public class m12 {
         // 계층 구조를 표현 가능
 
         아수라오리 a아수라오리 = new 아수라오리();
-        a아수라오리.a고무오리.날다();
-        a아수라오리.a오리.헤엄치다();
+        a아수라오리.a날개.날다();
+        a아수라오리.a오리발.헤엄치다();
 
 
     }
 }
 
 class 오리 {
-    오리 a오리 = new 오리();
-    고무오리 a고무오리 =new 고무오리();
+
     public void 날다() {
         System.out.println("오리가 날개로 날아갑니다.");
     }
@@ -53,8 +52,7 @@ class 흰오리 extends 오리 {
 }
 
 class 고무오리 extends 오리 {
-    오리 a오리 = new 오리();
-    고무오리 a고무오리 =new 고무오리();
+
 
     // 메서드 재정의(오버라이딩)
     public void 날다() {
@@ -72,9 +70,31 @@ class 고무2오리 extends 고무오리 {
 }
 
 class 아수라오리 {
-    오리 a오리 = new 오리();
-    고무오리 a고무오리 =new 고무오리();
+    날개 a날개 = new 날개();
+    오리발 a오리발 =new 오리발();
 
+
+
+
+}
+class 날개{
+    public void 날다() {
+        System.out.println("오리가 날개로 날아갑니다.");
+    }
+    public void 날다2() {
+        System.out.println("저는 날 수 없어요.. ㅠ");
+    }
+
+
+
+}
+class 오리발{
+    public void 헤엄치다() {
+        System.out.println("오리가 오리발로 헤엄칩니다.");
+    }
+    public void 헤엄치다2() {
+        System.out.println("오리가 오리발로 헤엄칩니다.");
+    }
 
 }
 
